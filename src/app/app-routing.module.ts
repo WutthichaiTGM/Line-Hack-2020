a6a742391adbe584f47316e0a20b9fb565dcbb17
WutthichaiTGM <wutthichai.sri.60@ubu.ac.th>
2020-10-15 22:49:52 +0700

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  providers: [{ provide: APP_BASE_HREF, useValue: "/my/app" }],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
